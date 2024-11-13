f196bc259c161b66195dcf6087569b3162f01c3d
Gianpo164 <167374385+Gianpo164@users.noreply.github.com>
2024-11-12 22:08:16 -0300

    Implementación de funciones de búsqueda de citas por paciente y especialista

--- DataBase/DataBase.py
+++ DataBase/DataBase.py
@@ -1,55 +1,59 @@
 import sqlite3
 
-con = sqlite3.connect("centro_medico.db")
-cursor = con.cursor()
+def crear_database():
+    db = open("centro_medico.db", "x")
+    db.close()
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Especialistas (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nombre TEXT NOT NULL,
-        especialidad TEXT NOT NULL
-)
-""")
+    con = sqlite3.connect("centro_medico.db")
+    cursor = con.cursor()
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Pacientes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nombre TEXT NOT NULL,
-        telefono TEXT,
-        direccion TEXT
-)
-""")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Especialistas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            especialidad TEXT NOT NULL
+    )
+    """)
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Citas (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        fecha TEXT NOT NULL,
-        hora TEXT NOT NULL,
-        id_especialista INTEGER,
-        id_paciente INTEGER,
-        FOREIGN KEY (id_especialista) REFERENCES Especialistas(id),
-        FOREIGN KEY (id_paciente) REFERENCES Pacientes(id)
-)
-""")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Pacientes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            telefono TEXT,
+            direccion TEXT
+    )
+    """)
 
-con.commit()
-con.close()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Citas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fecha TEXT NOT NULL,
+            hora TEXT NOT NULL,
+            id_especialista INTEGER,
+            id_paciente INTEGER,
+            FOREIGN KEY (id_especialista) REFERENCES Especialistas(id),
+            FOREIGN KEY (id_paciente) REFERENCES Pacientes(id)
+    )      
+    """)
 
-def insertar_especialista(nombre=str, especialidad=str):
+    con.commit()
+    con.close()
+
+def insertar_especialista(nombre:str, especialidad:str):
     con = sqlite3.connect("centro_medico.db")
     cursor = con.cursor()
     cursor.execute("INSERT INTO Especialistas (nombre, especialidad) VALUES (?, ?)", (nombre, especialidad))
     con.commit()
     con.close()
 
-def insertar_paciente(nombre=str, telefono=str, direccion=str):
+def insertar_paciente(nombre:str, telefono:str, direccion:str):
     con = sqlite3.connect("centro_medico.db")
     cursor = con.cursor()
     cursor.execute("INSERT INTO Pacientes (nombre, telefono, direccion) VALUES (?, ?, ?)", (nombre, telefono, direccion))
     con.commit()
     con.close()
 
-def insertar_cita(fecha=str, hora=str, id_especialista=int, id_paciente=int):
+def insertar_cita(fecha: str, hora: str, id_especialista:int, id_paciente:int):
     con = sqlite3.connect("centro_medico.db")
     cursor = con.cursor()
     cursor.execute("INSERT INTO Citas (fecha, hora, id_especialista, id_paciente) VALUES (?, ?, ?, ?)", (fecha, hora, id_especialista, id_paciente))
@@ -76,6 +80,22 @@ def obtener_datos_citas():
     con = sqlite3.connect("centro_medico.db")
     cursor = con.cursor()
     cursor.execute("SELECT * FROM Citas")
+    datos = cursor.fetchall()
+    con.close()
+    return datos
+
+def citas_pacientes(id_paciente):
+    con = sqlite3.connect("centro_medico.db")
+    cursor = con.cursor()
+    cursor.execute(f"SELECT * FROM Citas WHERE id_paciente = {id_paciente}")
+    datos = cursor.fetchall()
+    con.close()
+    return datos
+
+def citas_especialista(id_especialista):
+    con = sqlite3.connect("centro_medico.db")
+    cursor = con.cursor()
+    cursor.execute(f"SELECT * FROM Citas WHERE id_especialista = {id_especialista}")
     datos = cursor.fetchall()
     con.close()
     return datos
