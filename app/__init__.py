53eb77519a4a7a268588f7fc17f25f1428976b7d
kote-e <mariasanmartin2023@udec.cl>
2024-11-12 22:23:42 -0300

    REDISEÑO

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object('config')
 
     with app.app_context():
         # Importar rutas
